02d9cca94dbe828bb66f4bbbd9dc8c2730a6fa40
Anuja Tupe <anujatupe2012@gmail.com>
2014-12-27 15:44:17 -0800

    First commit - has record button, stop button and shows the label that it is recording

--- Voice Changer/ViewController.swift
+++ Voice Changer/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        // Hide the stop button
+        stopButton.hidden = true
+    }
 
 
+    @IBAction func stopRecordingAudio(sender: UIButton) {
+        recordingInProgress.hidden = true
+        //Enable the record button
+        recordButton.enabled = true
+    }
+    @IBAction func recordAudio(sender: UIButton) {
+        //Disable the record button
+        recordButton.enabled = false
+        //Show the stop button
+        stopButton.hidden = false
+        // Show text that the recording is in progress
+        recordingInProgress.hidden = false
+        // Record the voice
+        println("Inside Record Audio..")
+    }
 }
 
